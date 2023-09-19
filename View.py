46edfde40e422fce3fcf72bc92995d3af2ebca4f
ICSBMasterton <ICS21-126@camosun.bc.ca>
2023-09-19 10:19:08 -0700

    Added how to create board and text from the main, still need to add min and max value which will be rand values between 5 and 10

--- View.py
+++ View.py
@@ -3,6 +3,7 @@ from Board import Board
 from Treasure import Treasure
 from Tile import Tile
 
+
 def display(myBoard: Board):
     for row in myBoard.board:
         for tile in row:
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-import View
+from View import View
+from Board import Board
+
+# we need to create a board and pass it on to view, as board creates the tile
+# matrix with the randomly placed treasures and then view will iterate through it
+# and display them
+
+newBoard = Board(5, 10, )
+display(newBoard)
 
