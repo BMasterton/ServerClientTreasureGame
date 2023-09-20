a420748fd3b366a173d623d08c8fea4c7b3833fd
ICSBMasterton <ICS21-126@camosun.bc.ca>
2023-09-20 15:14:06 -0700

    Added treasure

--- Board.py
+++ Board.py
@@ -10,13 +10,13 @@ class Board:
         self.min_val = min_val
         self.max_val = max_val
         self.board = [[Tile() for _ in range(n)] for _ in range(n)]
-
+        for x in range(5):
+            randNumX = random.randrange(0,10)
+            randNumY = random.randrange(0, 10)
+            self.board[randNumX][randNumY].treasure = Treasure(5)
 #         need to somehow add bloody Treasure to random spots in the board i think after its been created
 
-for x in range(5):
-    randNumX = random.randrange(0,10)
-    randNumY = random.randrange(0, 10)
-    self.board[randNumX][randNumY].concat()
+
 
 
 
