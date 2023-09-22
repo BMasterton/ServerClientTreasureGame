17e2064a92a66c853425b978e1ae31840b950f0d
ICSBMasterton <ICS21-126@camosun.bc.ca>
2023-09-22 10:29:40 -0700

    Added treasure

--- Board.py
+++ Board.py
@@ -36,13 +36,50 @@ class Board:
        self.board[xCord][yCord].add_player(Player(name))
        self.players[name] = xCord, yCord
        print(self.players[name])
+
+
         #put this made player and coords into its own dictionary
 
-    def move_player(self, name, direction):
-        # need to check the location of the current player by nanme via the dictironary
+    def move_player(self, name, direction: char):
+        # need to check the location of the current player by name via the dictironary
+        # this is the x and y coord of a player the value of the key value players dict
+        currLocation = self.players[name]
+        print(list(self.players.keys())[list(self.players.values()).index(self.players[name])])
+        # this is the name of the player the key of the dict, based on the coords value
+        name = list(self.players.keys())[list(self.players.values()).index(self.players[name])]
+        print(name)
+
+        try:
+            match direction:
+                case 'u' | 'P':
+                    # move y coord up 1 +=1
+                # if the y coord is at the edge so 10 then you cannot go up
+                #throw valueError
+                case 'd' | 'D':
+                    # move y coors down 1 -=1
+                # throw valueError
+                    # if the y coord is at the edge so 0, then you cannot run that command
+                case 'l' | 'L':
+                    # move x coords down 1 -= 1
+                    # throw valueError
+                    # if the x coor is at the edge so 0, then you cannot run the command
+                case 'r' | 'R':
+                    # move x coords up 1 += 1
+                # throw valueError
+                    # if the x coord is at the edge so 10, then you cannot run the coomand
+                case 'q' | 'Q':
+                    # exit the program
+                case _:
+                    print()
+        except valueError as details:
+            print(str(details))
+
+
         # need to have a if else nightmare with u d l r
         # need to have a check for the sides of the map, if they are
-        #larger than 10 on the x and y and less than 1 on the x and y, you need to throw
+        # larger than 10 on the x and y and less than 1 on the x and y, you need to throw
+        # when moving player icon, it has to override the one its going too, and then rewrite the one it came from
+        # as a "." again
         # a ValueError saying no, and dont do the move.
 
 
