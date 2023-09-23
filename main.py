210616f7e46ff9459342fe165c437a7f9f35e727
ICSBMasterton <ICS21-126@camosun.bc.ca>
2023-09-23 00:49:27 -0700

    Added small loop to main to test moving player, got each player to move indepenently and properly, just need to add collision with other players and with treasure, also need to make the spawn of both players unique, still not working

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from View import display
 from Board import Board
 import random
 
-newBoard = Board(5, 10, 5, 10, 2) # creating the new board
+newBoard = Board(5, 10, 5, 10, 2) # creating the new boar
 randPlayerXPos = random.randrange(0, 10) # creating initial random x and y coords
 randPlayerYPos = random.randrange(0, 10)
 playerNames = ["1", "2"] # list of players that will be added
@@ -18,17 +18,18 @@ for player in playerNames:
         raise ValueError("space already occupied")
     newBoard.add_player(player, randPlayerXPos, randPlayerYPos)
 
-# going to have to add all the text and loops for adking for more commands and more here it will
-# have to repeadely the board
-for i in range(3):
+
+#
+for i in range(5):
     display(newBoard)
-    playerInputDirection = input(str('(U)p (D)own (L)eft (R)ight (Q)uit \n'))
-    playerInputPlayer = input(str('Player 1 or 2 \n'))
+    # need error checking for this one
+    playerInputDirection = input("(U)p (D)own (L)eft (R)ight (Q)uit \n")
+    playerInputPlayer = str(input("Player 1 or 2 \n"))
     #match may be better here
     if playerInputPlayer == '1':
-        Board.move_player(playerNames[0], playerInputDirection)
+        newBoard.move_player(playerNames[0], playerInputDirection)
     else:
-        Board.move_player(playerNames[1], playerInputDirection)
+        newBoard.move_player(playerNames[1], playerInputDirection)
 
 
 
