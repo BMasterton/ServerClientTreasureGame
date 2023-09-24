ccd93e0aea32cd9973c96b306b509df8e4bde23d
ICSBMasterton <ICS21-126@camosun.bc.ca>
2023-09-24 12:35:29 -0700

    Added collision checking and wrapper checking, as well as checking for spawn points of players.

--- main.py
+++ main.py
@@ -6,31 +6,39 @@ newBoard = Board(5, 10, 5, 10, 2) # creating the new boar
 randPlayerXPos = random.randrange(0, 10) # creating initial random x and y coords
 randPlayerYPos = random.randrange(0, 10)
 playerNames = ["1", "2"] # list of players that will be added
+playerDirections = ['U' , 'D' , 'L' , 'R' , 'Q', 'u' , 'd' , 'l' , 'r' , 'q']
 
 # goes over all the players in playerNames and looks at a random  x and y pos so it can add them, if that position is
 # not a "." then it will try again until it can, if the spot is taken it will let the player know
 for player in playerNames:
     randPlayerXPos = random.randrange(0, 10)
     randPlayerYPos = random.randrange(0, 10)
-    while newBoard.board[randPlayerXPos][randPlayerYPos].description != ".":
-        randPlayerXPos = random.randrange(0, 10)
-        randPlayerYPos = random.randrange(0, 10)
-        raise ValueError("space already occupied")
+    # making sure that if the tile already has a player or a treaure, another player cant spawn on them
+    while newBoard.board[randPlayerXPos][randPlayerYPos].treasure != None:
+        if newBoard.board[randPlayerXPos][randPlayerYPos].player != None:
+            randPlayerXPos = random.randrange(0, 10)
+            randPlayerYPos = random.randrange(0, 10)
+            raise ValueError("space already occupied")
     newBoard.add_player(player, randPlayerXPos, randPlayerYPos)
 
 
 #
-for i in range(5):
-    display(newBoard)
-    # need error checking for this one
-    playerInputDirection = input("(U)p (D)own (L)eft (R)ight (Q)uit \n")
-    playerInputPlayer = str(input("Player 1 or 2 \n"))
-    #match may be better here
-    if playerInputPlayer == '1':
-        newBoard.move_player(playerNames[0], playerInputDirection)
-    else:
-        newBoard.move_player(playerNames[1], playerInputDirection)
-
+while True:
+    try:
+        display(newBoard)
+        # need error checking for this one
+        playerInputDirection = input("(U)p (D)own (L)eft (R)ight (Q)uit \n")
+        if playerInputDirection not in playerDirections:
+            raise Exception('Must give a valid direction or quit')
+        playerInputPlayer = str(input("Player 1 or 2 \n"))
+        if playerInputPlayer not in playerNames:
+            raise Exception('value must be 1 or 2')
+        if playerInputPlayer == '1':
+            newBoard.move_player(playerNames[0], playerInputDirection)
+        elif playerInputPlayer == '2':
+            newBoard.move_player(playerNames[1], playerInputDirection)
+    except Exception as details:
+        print(str(details))
 
 
 
