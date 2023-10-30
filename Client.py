ea083628b0f9d4943caceab0b7d026e7c8d998bb
ICSBMasterton <ICS21-126@camosun.bc.ca>
2023-10-30 15:56:00 -0700

    refactored some code to send propely from packed values

--- Client.py
+++ Client.py
@@ -2,23 +2,18 @@ from socket import socket, AF_INET, SOCK_STREAM
 from struct import unpack
 import struct
 
-from sys import argv
-
 BUF_SIZE = 1024
 HOST = '127.0.0.1'
 PORT = 12345
 directionsString = ['U', 'D', 'L', 'R', 'Q', 'G']
 playerDirectionString = ['2', '3', '4', '6', '8', 'F']
 directionsBytes = [b'\x0010', b'\x0011', b'\x0100', b'\x0110', b'\x1000', b'\x1111']
-directionHeaderSize = 4
-endBuffer = b'\x00'
 HEADER_LEN = 2
 
 # prints the scores from the incoming recV() function from server
 def printScores(data):
-    # scores = sock.recv(BUF_SIZE) sock.revc(BUF_SIZE)would be passed into the function
-    score1 = unpack('!H', data[0:2])[0]
-    score2 = unpack('!H', data[2:4])[0]
+    score1 = unpack('!H', data[0:2])[0] #2:4 or 4:6 if 4 bytes is the number
+    score2 = unpack('!H', data[2:4])[0] #4:6 nut maune 6:8
     print('Score1', score1)
     print('Score2', score2)
     return
@@ -29,70 +24,154 @@ def printBoard(board):
     print(board)
     return
 
+def headerCheck( data):  # there will always be two initial bytes, which are the length header
+    if len(data) != 2:
+        raise Exception('Could not recieve Length of header!')
+    num_bytes = struct.unpack('!H', data)[0]  # unpack Data, and read the first (and only) unsigned int
+    print('numBytes', num_bytes)
+    bytes_read = 0
+    bytes_from_server = b''
+    while bytes_read < num_bytes:
+        next_bytes = data.recv( num_bytes - bytes_read)
+        bytes_read += len(next_bytes)
+        bytes_from_server += next_bytes
+    print(bytes_from_server)
+    return bytes_from_server
+
 #does some math to get a correct player number
 def playerInt(playerIntNumber):
     return playerIntNumber / 4
-
-def get_buf(current_socket: socket, expected_size: int) -> bytes:
-    current_size = 0
-    buffer = b''
-    while current_size < expected_size:
-        data = current_socket.recv(expected_size - current_size)
-        if data == b'':
-            return buffer
-        buffer = buffer + data
-        current_size = current_size + len(data)
-
-    return buffer
-
-def get_data(client: socket) -> bytes:
-    print('Client', client.getsockname(), 'waiting for data')
-    header = get_buf(client, HEADER_LEN)
-    print('Client', client.getsockname(), 'Header', header, header.hex())
-    data_len = unpack('!H', header)[0]
-    data = get_buf(client, data_len)
-    print('Client', client.getsockname(), 'Data', data, data.hex())
-    return data
 
 
 #when the server sends over the client id, for the rest of the connection the clinet will send back
 # every direction byte with the client number in bytes appended onto it. ie send should be like 00100100 still
 def main():
     with socket(AF_INET, SOCK_STREAM) as sock: # TCP socket
-        sock.connect((HOST, PORT)) # Initiates 3-way handshake
+        sock.connect((HOST, PORT)) # Initiates 3-way handsha
+        data_length_bytes = sock.recv(HEADER_LEN)
+        if len(data_length_bytes) != 2:
+            raise Exception('Could not recieve Length of header!')
+        num_bytes = struct.unpack('!H', data_length_bytes)[0]  # unpack Data, and read the first (and only) unsigned int
+        print('numBytes', num_bytes)
+        bytes_read = 0
+        bytes_from_server = b''
+        while bytes_read < num_bytes:
+            next_bytes = sock.recv(num_bytes - bytes_read)
+            bytes_read += len(next_bytes)
+            bytes_from_server += next_bytes
+            int_from_server = int.from_bytes(bytes_from_server, byteorder='big')
+            print('Intfromserver', int_from_server)
+        print('bytes from server',bytes_from_server)
 
         print('Client:', sock.getsockname()) # Client IP and port
-        get_data()
-        player_id_binary = sock.recv(BUF_SIZE) # getting info from the server for player id
-        print('BinaryID', player_id_binary)
-        player_hex_id = player_id_binary.hex()
-        print('HexID',player_hex_id)
-        player_int_number = int.from_bytes(player_id_binary, byteorder='big')
-        print(player_int_number)
+        print('BinaryID', bytes_from_server)
+        player_hex_id = bytes_from_server.hex()
+        player_int_number = int.from_bytes(bytes_from_server, byteorder='big')
+        print('Player ID', player_int_number)
 
         #player totallity checker
-        if playerInt(player_int_number) > 2:
+        if playerInt(int_from_server) > 2:
             print("too many clients closing the connection")
             return
 
         while True:
             directionInput = input("please enter a direction: ")
             if directionInput == directionsString[0]: #direction is U
-                sock.sendall(bytes.fromhex(playerDirectionString[0] + str(player_int_number)))
-                printScores(sock.recv(BUF_SIZE))
+                sock.sendall(directionsBytes[0] ) # sending the information back to the server
+                data_length_bytes = sock.recv(HEADER_LEN) # getting the length of the server sent over data
+                if len(data_length_bytes) != 2:
+                    raise Exception('Could not recieve Length of header!')
+                num_bytes = struct.unpack('!H', data_length_bytes)[0]  # unpack Data, and read the first (and only) unsigned int
+                bytes_read = 0
+                bytes_from_server = b''
+                while bytes_read < num_bytes:
+                    next_bytes = sock.recv(num_bytes - bytes_read)
+                    bytes_read += len(next_bytes)
+                    bytes_from_server += next_bytes
+                    int_from_server = int.from_bytes(bytes_from_server, byteorder='big')
+
+                printScores(sock.recv( num_bytes)) # print out the scores with the socket of the size returned from the header
             elif directionInput == directionsString[1]: #direction is D
                 sock.sendall(bytes.fromhex(playerDirectionString[1] + str(player_int_number)))
-                printScores(sock.recv(BUF_SIZE))
+                data_length_bytes = sock.recv(HEADER_LEN)
+                print('datalengthbytesDirU', data_length_bytes)
+                if len(data_length_bytes) != 2:
+                    raise Exception('Could not recieve Length of header!')
+                num_bytes = struct.unpack('!H', data_length_bytes)[
+                    0]  # unpack Data, and read the first (and only) unsigned int
+                print('numBytesDirectionU', num_bytes)
+                bytes_read = 0
+                bytes_from_server = b''
+                while bytes_read < num_bytes:
+                    next_bytes = sock.recv(num_bytes - bytes_read)
+                    bytes_read += len(next_bytes)
+                    bytes_from_server += next_bytes
+                    int_from_server = int.from_bytes(bytes_from_server, byteorder='big')
+                    print('IntfromserverDirectionU', int_from_server)
+                print('bytes from serverDirectionU', bytes_from_server)
+
+                printScores(sock.recv(num_bytes))
             elif directionInput == directionsString[2]: #direction is L
                 sock.sendall(bytes.fromhex(playerDirectionString[2] + str(player_int_number)))
-                printScores(sock.recv(BUF_SIZE))
+                data_length_bytes = sock.recv(HEADER_LEN)
+                print('datalengthbytesDirU', data_length_bytes)
+                if len(data_length_bytes) != 2:
+                    raise Exception('Could not recieve Length of header!')
+                num_bytes = struct.unpack('!H', data_length_bytes)[
+                    0]  # unpack Data, and read the first (and only) unsigned int
+                print('numBytesDirectionU', num_bytes)
+                bytes_read = 0
+                bytes_from_server = b''
+                while bytes_read < num_bytes:
+                    next_bytes = sock.recv(num_bytes - bytes_read)
+                    bytes_read += len(next_bytes)
+                    bytes_from_server += next_bytes
+                    int_from_server = int.from_bytes(bytes_from_server, byteorder='big')
+                    print('IntfromserverDirectionU', int_from_server)
+                print('bytes from serverDirectionU', bytes_from_server)
+
+                printScores(sock.recv(num_bytes))
             elif directionInput == directionsString[3]: # direction is R
                 sock.sendall(bytes.fromhex(playerDirectionString[3] + str(player_int_number)))
-                printScores(sock.recv(BUF_SIZE))
+                data_length_bytes = sock.recv(HEADER_LEN)
+                print('datalengthbytesDirU', data_length_bytes)
+                if len(data_length_bytes) != 2:
+                    raise Exception('Could not recieve Length of header!')
+                num_bytes = struct.unpack('!H', data_length_bytes)[
+                    0]  # unpack Data, and read the first (and only) unsigned int
+                print('numBytesDirectionU', num_bytes)
+                bytes_read = 0
+                bytes_from_server = b''
+                while bytes_read < num_bytes:
+                    next_bytes = sock.recv(num_bytes - bytes_read)
+                    bytes_read += len(next_bytes)
+                    bytes_from_server += next_bytes
+                    int_from_server = int.from_bytes(bytes_from_server, byteorder='big')
+                    print('IntfromserverDirectionU', int_from_server)
+                print('bytes from serverDirectionU', bytes_from_server)
+
+                printScores(sock.recv(num_bytes))
             elif directionInput == directionsString[4]: # direction is Q
                 # send info to server, print scores and board, quit
                 sock.sendall(bytes.fromhex(playerDirectionString[4] + str(player_int_number)))
-                printScores(sock.recv(BUF_SIZE))
+                data_length_bytes = sock.recv(HEADER_LEN)
+                print('datalengthbytesDirU', data_length_bytes)
+                if len(data_length_bytes) != 2:
+                    raise Exception('Could not recieve Length of header!')
+                num_bytes = struct.unpack('!H', data_length_bytes)[
+                    0]  # unpack Data, and read the first (and only) unsigned int
+                print('numBytesDirectionU', num_bytes)
+                bytes_read = 0
+                bytes_from_server = b''
+                while bytes_read < num_bytes:
+                    next_bytes = sock.recv(num_bytes - bytes_read)
+                    bytes_read += len(next_bytes)
+                    bytes_from_server += next_bytes
+                    int_from_server = int.from_bytes(bytes_from_server, byteorder='big')
+                    print('IntfromserverDirectionU', int_from_server)
+                print('bytes from serverDirectionU', bytes_from_server)
+
+                printScores(sock.recv(num_bytes))
                 printBoard(sock.recv(BUF_SIZE))
                 return
             elif directionInput == directionsString[5]: # direction is G
--- Game.py
+++ Game.py
@@ -11,76 +11,87 @@ playerDirectionDecimals = [2, 3, 4, 6, 8, 15]
 BUF_SIZE = 1024
 lock = Semaphore()
 playerCounter = 0
+connections = {}
 
+#packs up all the points and headers for the points to send over to the client
+def pointPack(newBoard):
+    score1_bin = struct.pack('!H', int(newBoard.printPlayerScore('1')))
+    score2_bin = struct.pack('!H', int(newBoard.printPlayerScore('2')))
+    scores_len = 4
+    scores_header = struct.pack('!H', scores_len)
+    return scores_header + score1_bin + score2_bin
+
+def boardPack(newBoard):
+    payload = newBoard.boardString().encode('utf-8')
+    payload_len = len(payload)
+    payload_header = struct.pack('!H', payload_len)
+    return payload_header + payload
+
+def playerPack(data):
+    data_length = struct.pack('!H', len(data))
+    data_to_send = data_length + data
+    return data_to_send
 
 def playerControl(sc, newBoard, playerNames):
     global playerCounter
+    playerId=0
     with sc:
         if playerCounter < 4: # setting player with playerCounter
             playerCounter += 1
         if playerCounter == 1:
-            sc.sendall(b'\x04')
+            #trying to somehow get each connections value
+            playerId = 1
+            data = b'\x04'
+            sc.sendall(playerPack(data)) #sending player data to the client
         elif playerCounter == 2:
-            sc.sendall(b'\x08')
-        elif playerCounter == 3:
-            sc.sendall(b'\x12')
+            playerId = 2
+            data = b'\x08'
+            sc.sendall(playerPack(data))
+        elif playerCounter == 3: # this player will still get sent through and cause the client to close
+            playerId = 3
+            data = b'\x12'
+            sc.sendall(playerPack(data))
             sc.close()
 
         while True:
             print('Client:', sc.getpeername())  # Client IP and port
             data = sc.recv(BUF_SIZE)  # us this to get info from client with 1 byte of info maybe make this BUD_SIZE
-            print('Data', data)
             data2 = list(data)
-            print('Data-2', data2)
             my_byte = data2[0]
-            print('MyByte',my_byte)
-            first_four_full = my_byte & 240
-            first_four_only = first_four_full >> 4
-            print(first_four_only)
-            middle_two_full = my_byte & 12
-            middle_two_only = middle_two_full >> 2
-            if first_four_only == playerDirectionDecimals[0]:  # direction Up
-                playerInputDirection = playerDirections[0] # setting playerInputDirection so the program knows its one of the allowed inputs
-                sc.sendall(
-                    struct.pack('!HH', newBoard.printPlayerScore('1'), newBoard.printPlayerScore('2'))) # sending the two scores in one stuct
-            elif first_four_only == playerDirectionDecimals[1]:  # direction Down
+            # becasue we only pass in the direction we no longer can get the player
+            first_four_full = my_byte & 15
+            if first_four_full == playerDirectionDecimals[0]:  # direction Up
+                playerInputDirection = playerDirections[0]
+                sc.sendall(pointPack(newBoard))
+            elif first_four_full == playerDirectionDecimals[1]:  # direction Down
                 playerInputDirection = playerDirections[1]
-                sc.sendall(
-                    struct.pack('!HH', newBoard.printPlayerScore('1'), newBoard.printPlayerScore('2')))
-            elif first_four_only == playerDirectionDecimals[2]:  # direction Left
+                sc.sendall(pointPack(newBoard))
+            elif first_four_full == playerDirectionDecimals[2]:  # direction Left
                 playerInputDirection = playerDirections[2]
-                sc.sendall(
-                    struct.pack('!HH', newBoard.printPlayerScore('1'), newBoard.printPlayerScore('2')))
-            elif first_four_only == playerDirectionDecimals[3]:  # direction Right
+                sc.sendall(pointPack(newBoard))
+            elif first_four_full == playerDirectionDecimals[3]:  # direction Right
                 playerInputDirection = playerDirections[3]
-                sc.sendall(
-                    struct.pack('!HH', newBoard.printPlayerScore('1'), newBoard.printPlayerScore('2')))
-            elif first_four_only == playerDirectionDecimals[
+                sc.sendall(pointPack(newBoard))
+            elif first_four_full == playerDirectionDecimals[
                 4]:  # when Q is received run send the scores of 1 and then 2 as shorts, send gameboard then itll run the quit command
                 playerInputDirection = playerDirections[4]
-                sc.sendall(struct.pack('!HH', newBoard.printPlayerScore('1'), newBoard.printPlayerScore('2')))
-                sc.sendall(newBoard.boardString().encode('utf-8'))
+                sc.sendall(pointPack(newBoard))
+                sc.sendall(boardPack(newBoard))
                 break
-            elif first_four_only == playerDirectionDecimals[
+            elif first_four_full == playerDirectionDecimals[
                 5]:  # when G is hit, print the scores, print the board, and then transmit the board
                 playerInputDirection = playerDirections[5]
                 with lock:
                     newBoard.printScore() # anything that is accessing newBoard and doing something on newboard we need to lock it so that its consistent
-                sc.sendall(struct.pack('!HH', newBoard.printPlayerScore('1'), newBoard.printPlayerScore('2')))
                 display(newBoard)
-                sc.sendall(newBoard.boardString().encode('utf-8'))
-            playerInputPlayer = str(middle_two_only)
+                sc.sendall(boardPack(newBoard))
             print('Data', data)  # printing the data we receive
 
             # making sure the directions and players are allowed choices or reject
             if playerInputDirection not in playerDirections:
                 raise Exception('Must give a valid direction or quit')
-            if playerInputPlayer not in playerNames:
-                raise Exception('value must be 1 or 2')
-            if playerInputPlayer == '1':
-                newBoard.move_player(playerNames[0], playerInputDirection)
-            elif playerInputPlayer == '2':
-                newBoard.move_player(playerNames[1], playerInputDirection)
+            #playerInputPlayer needs to be the connection number which would be 1 or 2
+            newBoard.move_player(playerId, playerInputDirection)
             display(newBoard)
 
 
@@ -93,7 +104,7 @@ class Game:
         playerNames = ["1", "2"]  # list of players that will be added
         HOST = ''
         PORT = 12345
-
+        thread_list = []
 
         # goes over all the players in playerNames and looks at a random  x and y pos so it can add them, if that position is
         # not a "." then it will try again until it can, if the spot is taken it will let the player know
@@ -106,11 +117,6 @@ class Game:
                 randPlayerYPos = random.randrange(0, 10)
             newBoard.add_player(player, randPlayerXPos, randPlayerYPos)
 
-        thread_list = []
-        # for i in range(2):
-        #     thread_list.append(Thread(target=playerControl, args=(i, )))
-        #     thread_list[-1].start()
-        #
 
 
         while True:
