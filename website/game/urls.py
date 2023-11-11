5c388f3b622dc109b54d24e4ed6aee6f871cc104
ICSBMasterton <ICS21-126@camosun.bc.ca>
2023-11-11 14:01:18 -0800

    adding some more paths, view functions etc

--- website/game/urls.py
+++ website/game/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.display, name='display'), # default path when people go to our site and look up 127.0.0.1/game/
     path('player/', views.get_all_players, name='players'),
     path('player/create/', views.PlayerCreate.as_view(), name='player_create'),
     path('player/update/<int:pk>/', views.PlayerUpdate.as_view(),name='player_update'),
+    path('create/', views.CreateGame.as_view(), name='create' ),
+   # path('display/<int:player_id>', views.displayPlayer),
 ]
